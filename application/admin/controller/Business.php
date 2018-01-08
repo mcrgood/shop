c2e99e203a304724a7b8c9f81e47f8525da71467
zhanghangfei <616709127@qq.com>
2018-01-08 10:35:29 +0800

    kongzhiq

--- application/admin/controller/Business.php
+++ application/admin/controller/Business.php
@@ -1,87 +1,23 @@
-<?php
-
+<?php 
 	namespace app\admin\controller;
-	use app\admin\controller\BaseController;
-	use think\Request;
+	use data\service\Address;
+	use data\service\Album;
+	use data\service\Express as Express;
+	use data\service\Supplier;
+	use Qiniu\json_decode;
+	use think\Config;
 
-class Business extends BaseController{
+	class Business extends BaseController{
 
-	public function lst(){
-		$ch = '';
-		$name = Request::instance()->session('name');
+		public function __construct(){
+        	parent::__construct();
+   		}
 
-		if (preg_match("/[\x7f-\xff]/", $name)) {  
-			$ch = $name; 
+		public function businesslist(){
+			//return $this->fetch();
+			return view($this->style . "Business/businesslist");
+			
 		}
-
-		$shopList = db("ns_goods_shop")->where(['state'=>['IN','1,2,3']])->paginate(20);
-
-		if ($ch!='') {
-
-			$shopList = db("ns_goods_shop")->where(['state'=>['IN','1,2,3'],'shi'=>['like',$ch.'%']])->paginate(20);
-		}
-
-		return $this->fetch('',['shopList'=>$shopList]);
 	}
 
-	public function edit(){
-
-		if (request()->isPost()) {
-
-			$data['id'] = input('post.id',0,'intval');
-
-			$shop = db('shop')->find($data['id']);
-
-			$cus = db('customer')->where('id',$shop['customer_id'])->find();
-
-			$data['name'] = input('post.name');
-
-			$data['description'] = input('post.description');
-
-			$data['state'] = input('post.state',1,'intval');
-
-			$data['jingdu'] = input('post.jingdu','');
-
-			$data['weidu'] = input('post.weidu','');
-
-			$update = db("ns_goods_shop")->update($data);
-
-			if ($data['state']==1 || $data['state']==3) {
-
-				db('customer')->where('id',$cus['id'])->update(['state'=>2]);
-
-			} else {
-
-				db('customer')->where('id',$cus['id'])->update(['state'=>3]);
-
-			}
-
-			if ($update) {
-
-				$this->success('修改商家信息成功！','lst');
-
-			}else{
-
-				$this->error('修改商家信息失败！','lst');
-
-			}
-
-		}
-
-		$shopId = input('id');
-
-		$shopInfo = db('shop')->find($shopId);
-
-		return $this->fetch('',['shopInfo'=>$shopInfo]);
-	}
-
-	public function reserve_lst(){
-		$reserve = db("reserve")->select();
-		$this->assign('reserve',$reserve);
-		return $this->fetch();
-	}
-
-	public function reserve_del(){
-		
-	}
-}
+ ?>
