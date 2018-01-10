07694752b20b29aaa918ca76a833c352b368cf7e
mcrgood <maocongren@sina.com>
2018-01-10 15:22:01 +0800

    测试

--- data/extend/org/wechat/Jssdk.php
+++ data/extend/org/wechat/Jssdk.php
@@ -139,7 +139,6 @@ class Jssdk {
       $url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=$accessToken";
       $res = json_decode($this->httpGet($url));
       $ticket = $res->ticket;
-      echo $ticket;exit;
       if ($ticket) {
         $data->expire_time = time() + 7000;
         $data->jsapi_ticket = $ticket;
