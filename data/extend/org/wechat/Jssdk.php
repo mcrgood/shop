b8cc42630eb87a34df4456459f0372af233bd81d
mcrgood <maocongren@sina.com>
2018-01-10 15:52:11 +0800

    测试

--- data/extend/org/wechat/Jssdk.php
+++ data/extend/org/wechat/Jssdk.php
@@ -160,8 +160,8 @@ class Jssdk {
       // $url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$this->appId&corpsecret=$this->appSecret";
       $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$this->appId&secret=$this->appSecret";
       $res = json_decode($this->httpGet($url));
+      var_dump($res);exit;
       $access_token = $res->access_token;
-      echo $access_token;exit;
       if ($access_token) {
         $data->expire_time = time() + 7000;
         $data->access_token = $access_token;
