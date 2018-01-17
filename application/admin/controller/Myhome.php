d08ddf2f491d96cf2bb3c323f673b4e2ce978eaa
zhanghangfei <616709127@qq.com>
2018-01-17 10:48:18 +0800

    预定管理

--- application/admin/controller/Myhome.php
+++ application/admin/controller/Myhome.php
@@ -3,6 +3,14 @@ namespace app\admin\controller;
 use Qiniu\json_decode;
 use think\Config;
 use think\Db;
+use data\service\Address;
+use data\service\Album;
+use data\service\Express as Express;
+use data\service\Goods as GoodsService;
+use data\service\GoodsBrand as GoodsBrand;
+use data\service\GoodsCategory as GoodsCategory;
+use data\service\GoodsGroup as GoodsGroup;
+use data\service\Supplier;
 
 class Myhome extends BaseController
 {
@@ -17,11 +25,21 @@ class Myhome extends BaseController
 
 	public function registerlist(){
 		$list = db("ns_goods_login")
-			->alias('a')
-			->join('ns_shop_message m','a.id=m.userid','LEFT')
-			->select();
+			 ->alias('a')
+			 ->join('ns_shop_message m','a.id=m.userid','LEFT')
+			 ->select();
 		$this->assign('list',$list);
 		return view($this->style . "Myhome/registerlist");
+	}
+
+	public function registerdetail(){
+		return view($this->style . "Myhome/registerdetail");
+	}
+
+	public function yuding(){
+		$list = db("ns_goods_reserve")->select();
+		$this->assign('list',$list);
+		return view($this->style . "Myhome/yuding");
 	}
 
 }
