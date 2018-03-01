594f3c4f80f52061120d3a5ea6fb6a4477d4aac1
zhanghangfei <616709127@qq.com>
2018-03-01 10:54:26 +0800

    商家下划线添加

--- application/wap/controller/Dingwei.php
+++ application/wap/controller/Dingwei.php
@@ -43,10 +43,9 @@ class Dingwei extends BaseController{
             }
             array_multisort(array_column($list, 'distance'), SORT_ASC, $list);
             foreach ($list as $key => $value) {
-                $str .= '<li><a href="' . url("catdetail", array("id" => $value['id'])) . '">' .
+                $str .= '<li style="border-bottom: 1px solid #cccccc;"><a href="' . url("catdetail", array("id" => $value['id'])) . '">' .
                     '<img src="' . $value['thumb'] . '" /><span>' . $value['name'] . '</span></a>
                 店名：' . $value['names'] . '<br/>地址：' . $value['address'] . '<br />距离：' . $value['distance'] . ' km <br />电话：' . $value['tel'] . '<a href="' . url('catdetail', array('id' => $value['id'])) . '" class="merchant-ul-a">>>更多详情</a></li>';
-
             }
             return ["message" => $str, "state" => 1];
         }
