eff62aec41052755b7014f3c3fffb0f13835483a
q419763874 <419763874@qq.com>
2018-03-23 13:01:52 +0800

    33

--- application/shop/controller/Fastpay.php
+++ application/shop/controller/Fastpay.php
@@ -50,10 +50,11 @@ class Fastpay extends Controller
 
 	//用户开户接口
 	public function user_open(){
+
 		 $reqIp = request()->ip();  //获取客户端IP
 		 $reqDate = date("Y-m-d H:i:s",time());
 	     $body="<body><merAcctNo>".$this->merAcctNo."</merAcctNo><userType>2</userType><customerCode>13657085273</customerCode><identityType>1</identityType><identityNo>360103198906283418</identityNo><userName>屈华俊</userName><legalName></legalName><legalCardNo></legalCardNo><mobiePhoneNo>13657085273</mobiePhoneNo><telPhoneNo></telPhoneNo><email></email><contactAddress></contactAddress><remark></remark><pageUrl>".$this->pageUrl."</pageUrl><s2sUrl>".$this->S2Snotify_url."</s2sUrl><directSell></directSell><stmsAcctNo></stmsAcctNo><ipsUserName>13657085273</ipsUserName></body>";
-	     $head ="<head><version>V1.0.1</version><reqIp>".$reqIp."</reqIp><reqDate>".$reqDate."</reqDate><signature>".md5($body.$this->MerCret)."</signature></head>";
+	     $head ="<head><version>v1.0.1</version><reqIp>".$reqIp."</reqIp><reqDate>".$reqDate."</reqDate><signature>".md5($body.$this->MerCret)."</signature></head>";
 	     $openUserReqXml="<openUserReqXml>".$head.$body."</openUserReqXml>";
 	     //加密请求类容
 	     $transferReq = $this->encrypt($openUserReqXml);
@@ -78,17 +79,17 @@ class Fastpay extends Controller
             return false;
         }
         
-        // $o = "";
-        // foreach ( $post_data as $k => $v )
-        // { 
-        //     $o.= "$k=" . urlencode( $v ). "&" ;
-        // }
-        // $post_data = substr($o,0,-1);
+        $o = "";
+        foreach ( $post_data as $k => $v )
+        { 
+            $o.= "$k=" . urlencode( $v ). "&" ;
+        }
+        $post_data = substr($o,0,-1);
         $postUrl = $url;
         $curlPost = $post_data;
         $ch = curl_init();//初始化curl
         curl_setopt($ch, CURLOPT_URL,$postUrl);//抓取指定网页
-        // curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
+        curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
         curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
         curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
