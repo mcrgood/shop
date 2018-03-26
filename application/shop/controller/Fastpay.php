8b6262e9b0a4b90f58deeaf36f1674d3473da590
q419763874 <419763874@qq.com>
2018-03-26 13:06:55 +0800

    易收付测试

--- application/shop/controller/Fastpay.php
+++ application/shop/controller/Fastpay.php
@@ -87,14 +87,14 @@ class Fastpay extends Controller
         }
         $post_data = substr($o,0,-1);
         $curlPost = $post_data;
-        $header[] = "Content-type: text/xml;charset=utf-8";
+        $postUrl = $url;
+        // $header[] = "Content-type: text/xml;charset=utf-8";
         $ch = curl_init();//初始化curl
-        curl_setopt($ch, CURLOPT_URL , $url);//抓取指定网页
-        curl_setopt($ch, CURLOPT_POST, true);//post提交方式
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_URL,$postUrl);//抓取指定网页
+        curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
         curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
-        curl_setopt($ch, CURLOPT_HEADER , 0);//设置header
 
         $data = curl_exec($ch);//运行curl
         curl_close($ch);
