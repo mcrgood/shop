c66d7e79f55518dc4e79fff60f6ba6316f01cc20
q419763874 <419763874@qq.com>
2018-03-26 16:52:46 +0800

    ysf(9)

--- application/shop/controller/Fastpay.php
+++ application/shop/controller/Fastpay.php
@@ -81,10 +81,8 @@ class Fastpay extends BaseController
 	    // $url = "http://127.0.0.1/shop/index.php/shop/fastpay/test";
 	    $post_data['ipsRequest'] = $ipsRequest;
 
-	    $xmls = $this->request_post($url, $post_data);
+	    $xmls = $this->request_post($url,$post_data);
 	    dump($reqIp);
-	    // echo '<br />';
-	    dump($reqDate);
 	    echo "这是明文 ：" . $xmls;die;
 	}
 
@@ -93,24 +91,24 @@ class Fastpay extends BaseController
 	  * 
 	  * */
   
- 	public function request_post($url = '', $post_data = array()) {
+ 	public function request_post($url = '',$post_data = array()) {
         if (empty($url) || empty($post_data)) {
             return false;
         }
         
-        $o = "";
-        foreach ( $post_data as $k => $v ) 
-        {
-            $o.= "$k=" . urlencode( $v ). "&" ;
-        }
-        $post_data = substr($o,0,-1);
+        // $o = "";
+        // foreach ( $post_data as $k => $v ) 
+        // {
+        //     $o.= "$k=" . urlencode( $v ). "&" ;
+        // }
+        // $post_data = substr($o,0,-1);
         $postUrl = $url;
         $curlPost = $post_data;
         $ch = curl_init();//初始化curl
         curl_setopt($ch, CURLOPT_URL,$postUrl);//抓取指定网页
-        curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
-        curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);//要求结果为字符串且输出到屏幕上
+        // curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
+        curl_setopt($ch, CURLOPT_POST, true);//post提交方式
         curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
         $data = curl_exec($ch);//运行curl
         curl_close($ch);
