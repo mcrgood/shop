a2cc884e1a351b8ea5414a0318d3868f586564ed
mcrgood <maocongren@sina.com>
2018-04-01 11:16:12 +0800

    支付测试

--- application/shop/controller/Fastpay.php
+++ application/shop/controller/Fastpay.php
@@ -317,12 +317,12 @@ class Fastpay extends BaseController
         $reqDate = date("Y-m-d H:i:s",time());
         $body="<body><merAcctNo>".$this->merAcctNo."</merAcctNo><userType>2</userType><customerCode>13657085273</customerCode><identityType>1</identityType><identityNo>52212619930930551X</identityNo><userName>隔壁老王</userName><legalName></legalName><legalCardNo></legalCardNo><mobiePhoneNo>13657085273</mobiePhoneNo><telPhoneNo></telPhoneNo><email></email><contactAddress></contactAddress><remark></remark><pageUrl>".$this->pageUrl."</pageUrl><s2sUrl>".$this->S2Snotify_url."</s2sUrl><directSell></directSell><stmsAcctNo></stmsAcctNo><ipsUserName>13657085273</ipsUserName></body>";
         $head ="<head><version>v1.0.1</version><reqIp>".$reqIp."</reqIp><reqDate>".$reqDate."</reqDate><signature>".md5($body.$this->MerCret)."</signature></head>";
-        $openUserReqXml="<?xml version='1.0' encoding='utf-8'?><openUserReqXml>".$head.$body."</openUserReqXml>";
+        $openUserReqXml="<openUserReqXml>".$head.$body."</openUserReqXml>";
         //加密请求类容
         $transferReq = $this->encrypt($openUserReqXml);
         //拼接$ipsRequest
 
-        $ipsRequest = "<?xml version='1.0' encoding='utf-8'?><ipsRequest><argMerCode>".$this->argMerCode."</argMerCode><arg3DesXmlPara>".$transferReq."</arg3DesXmlPara></ipsRequest>";
+        $ipsRequest = "<ipsRequest><argMerCode>".$this->argMerCode."</argMerCode><arg3DesXmlPara>".$transferReq."</arg3DesXmlPara></ipsRequest>";
 
 
         $post_data = $ipsRequest;
