94cf41ef79d50fc4ddfa14ceab9188885e70064b
q419763874 <419763874@qq.com>
2018-07-07 18:33:25 +0800

    333

--- application/wap/controller/WeiShangPay.php
+++ application/wap/controller/WeiShangPay.php
@@ -32,18 +32,18 @@ class WeiShangPay extends BaseController
 	//微信支付API
 	public function wx_pay_api(){
 		$out_trade_no = input('param.out_trade_no',0); //获取地址栏订单号
+		$coupon_id = input('param.coupon_id'); //优惠券金额
 		if($out_trade_no == 0){
 			$this->error('订单参数错误，请重新提交！');
 		}
 		$row = db('ns_order_payment')->where('out_trade_no',$out_trade_no)->find(); //获取付款方式
-		$pay_money = $row['pay_money']-$coupon_money;
 		
-		$coupon_id = input('param.coupon_id'); //优惠券金额
 		if($coupon_id != 0){
 			$coupon_money = Db::table('ns_coupon')->where('coupon_id',$coupon_id)->value('money');
 		}else{
 			$coupon_money = 0;
 		}
+		$pay_money = $row['pay_money']-$coupon_money;
 		Db::table('ns_order_payment')->where('out_trade_no',$out_trade_no)->update(['real_pay_money' =>$pay_money, 'coupon_id' =>$coupon_id]);
 
 		if($row['type'] == 1){ //线上商城订单
